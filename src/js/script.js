44626201909e7e351be1597e3ec27410f1f8fabb
anya169 <oxina.anya@yandex.ru>
2026-04-09 21:46:35 +0700

    Напишем функцию добавления цифр и операторов на дисплей. Чтобы каждая кнопка добавляла на экран свой символ, привяжем к ним слушатели событий, в аргументы которых будет передавать два значения: событие, по которому будет срабатывать слушатель - click (клик по кнопке) и функцию, которая будет срабатывать после того, как произойдет событие - функция appendSymbol с аргументом, равным символу на кнопке. Обратим внимание, что пользователь не может вводить несколько нулей в начале выражения, а также несколько операторов.

--- src/js/script.js
+++ src/js/script.js
@@ -26,3 +26,53 @@ const buttonEquals = document.getElementById('button-equals');
 
 // Кнопка "Стереть"
 const buttonDelete = document.getElementById('button-delete');
+
+
+// Функция добавления символа
+function appendSymbol(symbol) {
+    //Определяем массив с операторами
+    const operators = ['+', '-', '*', '/'];
+
+    // Если на экране ноль - заменяем значение на число
+    // Это работает в двух случаях:
+    // На экране ноль, который отображается по умолчанию в начале
+    // Пользователь вводит несколько нулей в начале, но мы помним, что числа с нуля не начинаются
+    if (display.value === '0') {
+        // Заменяем ноль на введеный символ, только если это число - операция не может начинаться с оператора
+        // Проверяем, что вводимый символ - не оператор
+        if (!operators.includes(symbol)) {
+            display.value = symbol;
+        }
+    // Если уже что-то введено, добавляем символ к строке в дисплее
+    // Помним, что мы работаем со строками, поэтому в нашем случае "2" + "3" = "23", а не "5"
+    } else {
+        // Проверяем, чему равен последний символ, чтобы не дать пользователю ввести несколько операторов подряд
+        let lastSymbol = display.value[display.value.length - 1];
+        // Проверяем, что последний и вводимый символы - не операторы
+        if (operators.includes(lastSymbol) && operators.includes(symbol)){
+            // Если так, то обрезаем последний введеный оператор и добавляем новый вводимый
+            display.value = display.value.slice(0, -1) + symbol;
+        } else {
+            // Если нет, то добавляем цифру на дисплей
+            display.value += symbol;
+        }
+    } 
+}
+
+// Привязываем кнопки цифр
+button0.addEventListener('click', function() { appendSymbol('0'); });
+button1.addEventListener('click', function() { appendSymbol('1'); });
+button2.addEventListener('click', function() { appendSymbol('2'); });
+button3.addEventListener('click', function() { appendSymbol('3'); });
+button4.addEventListener('click', function() { appendSymbol('4'); });
+button5.addEventListener('click', function() { appendSymbol('5'); });
+button6.addEventListener('click', function() { appendSymbol('6'); });
+button7.addEventListener('click', function() { appendSymbol('7'); });
+button8.addEventListener('click', function() { appendSymbol('8'); });
+button9.addEventListener('click', function() { appendSymbol('9'); });
+
+// Привязываем кнопки операций
+buttonAddition.addEventListener('click', function() { appendSymbol('+'); });
+buttonSubtraction.addEventListener('click', function() { appendSymbol('-'); });
+buttonMultiplication.addEventListener('click', function() { appendSymbol('*'); });
+buttonDivision.addEventListener('click', function() { appendSymbol('/'); });
